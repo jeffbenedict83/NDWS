550926ac7ec1c7de875f2cccff8084a408dee74c
jeffbenedict83 <jeffbenedict83@gmail.com>
2014-04-05 22:30:11 -0600

    first git commit ever!

---
+++ main/java/com/NDWS/common/controller/NDWSController.java
@@ -0,0 +1,46 @@
+package com.NDWS.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeffreyb08
+ * Date: 4/5/14
+ * Time: 9:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class NDWSController {
+    /*@RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        return "hello";
+    }*/
+
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+
+    }
+}
